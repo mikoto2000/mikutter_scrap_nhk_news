e6a9ade4cbbafbd2f8e776711174f887c9f93ae9
mikoto2000 <mikoto2000@gmail.com>
2014-08-25 05:16:51 +0900

    定数が重複するみたいなので回避。

--- scrap_nhk_news.rb
+++ scrap_nhk_news.rb
@@ -4,10 +4,10 @@ require "nokogiri"
 require "open-uri"
 
 Plugin.create(:scrap_nhk_news) do
-    ELEMENT_TITLE = 'span.contentTitle'
-    ELEMENT_MAIN = 'p#news_textbody,p#news_textmore'
-    ELEMENT_ADD = 'div.news_add *'
-    MESSAGE_SELECTORS = [ELEMENT_MAIN, ELEMENT_ADD]
+    @ELEMENT_TITLE = 'span.contentTitle'
+    @ELEMENT_MAIN = 'p#news_textbody,p#news_textmore'
+    @ELEMENT_ADD = 'div.news_add *'
+    @MESSAGE_SELECTORS = [@ELEMENT_MAIN, @ELEMENT_ADD]
 
     filter_rebuild_message do |message|
         dummy_message = message
@@ -39,9 +39,9 @@ Plugin.create(:scrap_nhk_news) do
         url = URI.extract(message.to_s)[0]
         doc = Nokogiri::HTML(open(url))
 
-        text = "■ #{doc.css(ELEMENT_TITLE)[0].text}\n"
+        text = "■ #{doc.css(@ELEMENT_TITLE)[0].text}\n"
 
-        for message_selector in MESSAGE_SELECTORS do
+        for message_selector in @MESSAGE_SELECTORS do
             elements = doc.css(message_selector)
 
             # 取得した要素に応じてテキスト変換
